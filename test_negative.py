0f68195e77156fb42b0cbd0947bbc7c7d4ef66a3
6705142030-design <6705142030@siam.edu>
2026-09-14 09:57:58 +0700

    Initial commit of testcase-lab

--- test_negative.py
+++ test_negative.py
@@ -24,3 +24,21 @@ def test_negative_age_rejected():
 def test_age_as_string_rejected():
     with pytest.raises(TypeError):
         validate_age("twenty")
+
+
+
+def test_email_without_at_rejected():
+    with pytest.raises(ValueError):
+        validate_email("notanemail.com")
+
+def test_email_without_domain_rejected():
+    with pytest.raises(ValueError):
+        validate_email("user@")
+
+def test_negative_age_rejected():
+    with pytest.raises(ValueError):
+        validate_age(-5)
+
+def test_age_as_string_rejected():
+    with pytest.raises(TypeError):
+        validate_age("twenty")
